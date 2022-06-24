be9278697818365820cc8b2ff4ba44f78cd0229d
p-vasic <vasicdeveloperbg@gmail.com>
2022-06-24 19:12:57 +0200

    feat: Add styled-components and react-router

---
+++ src/theme/light.ts
@@ -0,0 +1,11 @@
+import { IThemeMode } from "./model";
+
+const theme: IThemeMode = {
+    id: 'light',
+    bgColor: "#F5F6FA",
+    cardColor: "rgb(255,255,255)",
+    inputColor: "rgba(238,238,238, 0.8)",
+    textColor: "rgb(85, 85, 85)",
+}
+
+export default theme;
